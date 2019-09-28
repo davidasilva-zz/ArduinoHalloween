d1a3a732c7375053bb50228e1fcb05f9a9d3657c
davisilvah <davisilvah@gmail.com>
2019-09-28 15:34:08 -0700

    v3

--- main.cpp
+++ main.cpp
@@ -11,8 +11,7 @@ bool state = IDLE;
 float distance = 0;
 
 void run() {
-	 Serial.println("Running");
-
+	Serial.println("Running");
     Serial.println("Working...");
 }
 
@@ -35,38 +34,15 @@ void loop() {
 
 void setup() {
 
-    pinMode(8, OUTPUT);
+    pinMode(3, OUTPUT);
+    pinMode(5, OUTPUT);
+    pinMode(6, OUTPUT);
 
-    float c1 = 0.1;
-    float c2 = 0.5;
-    float c3 = 1.0;
-
-    float tc = 1000;
-
-    float tl1 = c1 * tc;
-    float td1 = tc - tl1;
-
-    float tl2 = c2 * tc;
-    float td2 = tc - tl2;
-
-    float tl3 = c3 * tc;
-    float td3 = tc - tl3;
-
-    for (;;) {
-    	digitalWrite(5, HIGH);
-    	delay((unsigned long)tl1);
-    	digitalWrite(5, LOW);
-    	delay((unsigned long)td1);
-
-    	digitalWrite(6, HIGH);
-    	delay((unsigned long)tl2);
-    	digitalWrite(6, LOW);
-    	delay((unsigned long)td2);
-
-    	digitalWrite(7, HIGH);
-    	delay((unsigned long)tl3);
-    	digitalWrite(7, LOW);
-    	delay((unsigned long)td3);
+    for(;;) {
+    	analogWrite(3, random(255));
+        analogWrite(5, random(255));
+        analogWrite(6, random(255));
+		delay(random(40, 500));
     }
 }
 
